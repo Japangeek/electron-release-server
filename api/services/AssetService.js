188ee58e6cd435427a8e9dc19791d08c0a990fcd
Japangeek <geek@gmail.com>
2019-04-10 01:12:34 +0200

    ...

--- api/services/AssetService.js
+++ api/services/AssetService.js
@@ -76,15 +76,15 @@ AssetService.getHash = function (fd, type = 'sha1') {
   return new Promise(function (resolve, reject) {
 
     var hash = crypto.createHash(type);
-    hash.setEncoding('base64');
-    var fd
+    hash.setEncoding('hex');
+
     var fileStream = fsx.createReadStream(fd)
       .on('error', function (err) {
         reject(err);
       })
       .on('end', function () {
         hash.end();
-        resolve(hash.read());
+        resolve(String.prototype.toUpperCase.call(hash.read()));
       })
       // Pipe to hash generator
       .pipe(hash);
